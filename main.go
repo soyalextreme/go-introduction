9871d7b4cf7ef59d665368ee8cd4c90f522baa48
Alejandro Andrade <alejandroandrade@MacBook-Air-de-Alejandro.local>
2020-02-19 20:09:51 -0600

    Primer programa en GO

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+
+import "fmt" 
+
+
+
+func main () {
+    
+    var name string 
+    fmt.Print("Ingresas tu nombre: ")
+    fmt.Scanf("%s", &name)
+    fmt.Print("Hola mundo ", name)
+}
